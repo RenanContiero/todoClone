70f1c0000d10d86c9d4e17413be301a182721ef0
Lucas Alexandre <61552617+lucasdev10@users.noreply.github.com>
2020-04-23 22:36:37 -0300

    Input melhorado (quando clica em add o valor do input apaga)

--- todoListApp/src/app/additem/additem.component.ts
+++ todoListApp/src/app/additem/additem.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from '../models/todo';
+import { R3TargetBinder } from '@angular/compiler';
 
 @Component({
   selector: 'app-additem',
@@ -8,38 +9,39 @@ import { Todo } from '../models/todo';
 })
 export class AdditemComponent implements OnInit {
 
-  
+
   // @Input() valorAtualizado:any; 
   // novoObjeto = {}
 
-  nomeTarefa:string;
-  controle:number = 0
+  nomeTarefa: string;
+  controle: number = 0
 
   constructor() { }
 
-  ngOnInit(): void { 
+  ngOnInit(): void {
 
   }
 
   todos: Todo[] = []
 
-  guardaTarefa(event){
-    this.nomeTarefa = event.target.value
-   
+  guardaTarefa(event) {
+    this.nomeTarefa = event.target.value;
+    event.target.value = ""
   }
 
   //adiciona o item como o primeiro do array
-  adicionarItem(event){
-   if (this.nomeTarefa == undefined || this.nomeTarefa == ""){
-     alert("Digite uma tarefa")
-   }else{
-     let nome = this.nomeTarefa;
-     let status = false;
-     let id:number = +1
-     this.todos.push(new Todo(id,nome, status));
+  adicionarItem(event) {
+    if (this.nomeTarefa == undefined || this.nomeTarefa == "") {
+      alert("Digite uma tarefa")
+    } else {
+      let nome = this.nomeTarefa;
+      let status = false;
+      let id: number = +1
+      this.todos.push(new Todo(id, nome, status));
+     
 
-   }
-  } 
+    }
+  }
   // atualizarItem(){
   //   let status = false;
   //   this.novoObjeto = new Todo(this.valorAtualizado, status);
